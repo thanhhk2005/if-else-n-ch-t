8419295914199a77bcffd0a788e32506db31362c
ThanhTran20 <89896792+thanhhk2005@users.noreply.github.com>
2021-12-14 12:35:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+
+from colorama import Fore
+mnb=Fore.BLUE
+mxanh=Fore.GREEN
+mdo=Fore.RED
+mtrang=Fore.WHITE
+mvang=Fore.YELLOW
+dung=delay=mdo+"kiểm tra độ nghiện"
+phuongle=delay=mvang+"Tool_By_:Thazh"
+print(dung)
+print(phuongle)
+def trangyennghia(thanhyeugiang):
+  print("đang update")
+yyy="thanhtool"
+print("\n")
+print(mdo+f"Nhâp Key")
+while True:
+  yyy=input(mxanh+f"Nhập {mvang}Key {mdo}Để {mxanh} vào{mvang} Tool{mdo} : "+mdo)
+  if yyy=="thanhtool":
+    pass
+    break
+  else:
+    print(mnb+"Vui Lòng Nhập Đúng")
+
+z=delay=mnb+"""
+
+╱╭╮╭╮╱╱╱╱╱╱╱╭╮╱╱╱╭╮╱╱╱╱╱╱╭
+╭╯╰┫┃╱╱╱╱╱╱╱┃┃╱╱╭╯╰╮╱╱╱╱╱┃┃
+╰╮╭┫╰━┳━━┳━╮┃╰━╮╰╮╭╋━━┳━━┫┃
+╱┃┃┃╭╮┃╭╮┃╭╮┫╭╮┃╱┃┃┃╭╮┃╭╮┃┃
+╱┃╰┫┃┃┃╭╮┃┃┃┃┃┃┃╱┃╰┫╰╯┃╰╯┃╰╮
+╱╰━┻╯╰┻╯╰┻╯╰┻╯╰╯╱╰━┻━━┻━━┻━╯"""
+print(z)
+import random 
+import time
+for i in range(5,0,-1):
+	print(i,end="...",flush='True')
+	time.sleep(1)
+b = random.randint(0,100)
+for i in range(5,0,-1):
+	print(i,end="\n",flush='True')
+	time.sleep(1)
+thanh=delay=mdo+"""
+ ___________
+|██_________| """
+print(thanh)
+print("⏳\n"*3)
+for i in range(5,0,-1):
+	print(i,end="\n",flush='True')
+	time.sleep(1)
+hoangngan=delay=mvang+f"""\t
+ ___________
+|██{mdo}_██{mxanh}_██{mtrang}___| """
+print(hoangngan)
+print("⏳\n"*3)
+for i in range(5,0,-1):
+	print(i,end="\n",flush='True')
+	time.sleep(1)
+nhunggay=delay=mtrang+f"""\t
+ ___________
+|██_{mdo}██{mvang}_██{mnb}_██|
+----100%------ """
+print(nhunggay)
+for i in range(5,0,-1):
+	print(i,end="\n",flush='True')
+	time.sleep(1)
+print("s̳u̳c̳c̳e̳s̳s̳e̳d̳")
+print(z)
+
+print("💀","đây là % nghiện của bạn:",b,"%")
+a=(int(input("nhập % nghiện của bạn vừa được đo:")))
+if a<20 :
+    print("bạn không bị nghiện")
+    print("❤chúc mừng bạn")
+elif a<60 :
+    print("bạn sắp nghiện")
+    print("❥bỏ đi làm người \n đi cai nghiện là vừa")
+else:
+    print("bạn nghiện hết thuốc chữa rồi","☠")
